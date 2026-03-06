4bf032328a1584e72c211d8881b81b5486704d73
Ruben <ruben@example.com>
2026-03-06 09:52:09 -0500

    6-3-26

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,3 +1,4 @@
-const moment = require('moment');
-moment.locale('es');
-console.log("Inicio de la aplicacion de nodejs con nodemon a las "+moment().format("[Hoy dia es] dddd"))
+//const moment = require('moment');
+//moment.locale('es');
+//console.log("Inicio de la aplicacion de nodejs con nodemon a las "+moment().format("[Hoy dia es] dddd"))
+
