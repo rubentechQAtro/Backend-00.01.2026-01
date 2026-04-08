50bd2ced708a458c90f6893718f9ce7a1802879e
Roberto Pineda <rpineda@x-codec.net>
2026-04-07 20:51:55 -0500

    Clase 7042026 primera parte

---
+++ Semana12Sesion01/d19423/readme.md
@@ -0,0 +1,8 @@
+ npx tsc --init
+ npm i -D nodemon
+ npx nodemon src/index.ts
+ npm i -D ts-node
+ npx nodemon src/index.ts
+ npm run dev
+ npm run build
+ npm run dev
---
+++ Semana12Sesion01/d19423/src/index.ts
@@ -0,0 +1,22 @@
+console.log("Inicio de la aplicacion");
+console.log("Inicio de la con nodemon");
+
+let bandera: boolean = true;
+
+bandera = false;
+
+let nombre: string = "Roberto";
+
+let edad: Number = 43;
+
+let apellido = "Pineda";
+
+let estadoCivil: any = {causa:"Es complicado"};
+
+estadoCivil = "Casado";
+
+estadoCivil = 0;
+
+let vidaSocial: unknown = "Tengo muchos amigos"
+
+let perro: null;
